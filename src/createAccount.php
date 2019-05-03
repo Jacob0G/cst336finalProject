ed1457b55bddde714ba97264958e87b7aa03ca12
Salihullah <salihabid56@yahoo.com>
2019-05-03 16:02:20 -0700

    Create Account
    Pushing create account files

--- src/createAccount.php
+++ src/createAccount.php
@@ -4,24 +4,142 @@
   <meta charset="UTF-8">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.9/css/all.css" integrity="sha384-5SOiIsAziJl6AWe0HWRKTXlfcSHKmYV4RBF18PPJ173Kzn7jzMyFuTtk8JA7QQG1" crossorigin="anonymous">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <!--<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>-->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
   <link href="../css/styles.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
   
-<!--NavBar top-->
     <script>
-     $(document).ready(function(){  
-
-        $("#navSearch").on("click", function(){
-          alert($("#searchInput").val());
-        });
+    /*global $*/
+    
+     $(document).ready(function(){ 
+       var check = true;
+       
+        $("#username").on("change", function() {//check if the username already exist in the Database
+          
+          $.ajax({
+                  type: "GET",
+                  url: "../api/createAccountAPI/checkUsername.php",
+                  dataType: "text", //is it json if i don't json encode ? 
+                  data: { "username": $('#username').val()
+                          
+                  },
+                  success: function(data,status) {
+                  //alert(data);
+                  //console.log(data);
+                  if(data == "false"){
+                    $("#usernameError").html("Username available"); 
+                    $("#usernameError").css("color", "blue");
+                    check = true;
+                  }
+                  else{
+                        alert("username is not available")
+                        $("#usernameError").html("Username already exist"); 
+                        $("#usernameError").css("color", "red");
+                        check = false;
+                      // window.location.href = "/cst336finalProject/src/createAccount.php"
+                  }
+                  
+                  },//success function
+                  complete: function(data,status) { //optional, used for debugging purposes
+                  //alert(status);
+                  }
+              });//ajax Call 
+            
+            }); // Username onchange event 
+    
+    
+        $("#email").on("change", function() {// check if email already exist in the Database
         
+          $.ajax({
+                  type: "GET",
+                  url: "../api/createAccountAPI/checkEmail.php",
+                  dataType: "text", //is it json if i don't json encode ? 
+                  data: { "email": $('#email').val()
+                          
+                  },
+                  success: function(data,status) {
+                  //alert(data);
+                  console.log(data);
+                  if(data == "false"){
+                  check = true;
+                  }
+                  else{
+                      alert("This email is associated with another account")
+                       $("#emailError").html("Email already exist"); 
+                       $("#emailError").css("color", "red");
+                       check = false;
+                  }
+                  
+                  },
+                  complete: function(data,status) { //optional, used for debugging purposes
+                  //alert(status);
+                  }
+              });//ajax
+              
+          }); // end event 
+    
+        $("#signupbtn").on("click", function(event){
+          
+            var fullname =  $("#fullname").val();
+            var email =  $("#email").val();
+            var username = $("#username").val();
+            var password = $("#password").val();
+            var cpassword = $("#cpassword").val();
+            event.preventDefault();
+              
+              if(fullname == '' || email == '' || username == ''|| password == ''){
+                alert("All fields need to be filled");
+                } 
+                else if ((password.length)<8){
+                 alert("Password should be 8 or more characters");
+                }
+                else if (password != cpassword) {
+                  alert("Your password doesn't match");
+                }
+                else if (!(check)){
+                  alert("Please fix the errors!");
+                }
+                else{
+                  
+                     $.ajax({
+                            type: "GET",
+                            url: "../api/createAccountAPI/registerAccount.php",
+                            dataType: "text", //is it json if i don't json encode ? 
+                            data: { "fullname": $("#fullname").val(),
+                                    "email": $("#email").val(),
+                                    "username": $("#username").val(),
+                                    "password": $("#password").val()
+                            },
+                           
+                            success: function(data,status) {
+                              //console.log(data);
+                              if(data == "Invalid"){
+                                $("#emailError").html("Invalid email");
+                                $("#emailError").css("color", "#f90202");
+                              }
+                              else {
+                                alert("Your account created successfully");
+                                window.location.href = "/cst336finalProject/src/login.php"
+                              }
+                             // window.location.href = "/cst336finalProject/src/login.php"
+                            //alert(data);
+                            },
+                            complete: function(data,status) { //optional, used for debugging purposes
+                            //alert(status);
+                            }
+                        });//ajax
+                              }
+                });//onclick signup button
         
-     });
+     });//document ready
+  
   </script>
+
+  <!--NavBar top-->
  <nav class="navbar navbar-light navbar-expand-md bg-faded justify-content-center align-items-center top">
     <!-- Navbar brand -->
     <div class="top_Nav"style="width=100%">
@@ -69,27 +187,26 @@
  
   <div class="container">
     <h1>Sign Up</h1>
+    
     <p>Please fill in this form to create an account.</p>
     
-      
-    <label for="firstName"><b>First Name</b></label>
-    <input type="text" placeholder="First Name" name="firstName" id = "firstName" required>
-    
-    <label for="lastName"><b>Last Name</b></label>
-    <input type="text" placeholder="Last Name" name="lastName" id = "lastName" required>
+    <label for="name"><b>Full Name</b></label>
+    <input type="text" placeholder="Full Name" name="fullname" id = "fullname" required>
     
     <label for="userName"><b>Username</b></label>
-    <input type="text" placeholder="Enter Username" name="userName" id = "userName" required>
+    <input type="text" placeholder="Enter Username" name="username" id = "username" required>
+    <div id="usernameError"></div> <br />
           
 
     <label for="email"><b>Email</b></label>
     <input type="text" placeholder="Enter Email" name="email" id = "email" required>
+    <span id="emailError"></span> <br />
 
     <label for="psw"><b>Password</b></label>
-    <input type="password" placeholder="Enter Password" name="psw" required>
+    <input type="password" placeholder="Enter Password" name="password" id = "password"  required>
 
-    <label for="psw-repeat"><b>Repeat Password</b></label>
-    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
+    <label for="psw-confirm"><b>Confirm Password</b></label>
+    <input type="password" placeholder="Confirm Password" name="cpassword" id = "cpassword" required>
 
     <label>
       <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
@@ -100,9 +217,15 @@
     <div class="clearfix">
       <form action="login.php">
          <button type="submit" class="cancelbtn">Cancel</button>
+         <button type="submit" class="signupbtn" id = "signupbtn">Sign Up</button>
+         
       </form>
+       
+          
+          
+     
       <!--<button type="button" class="cancelbtn"></button>-->
-      <button type="submit" class="signupbtn">Sign Up</button>
+   
     </div>
   </div>
 </form>
